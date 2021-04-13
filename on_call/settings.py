5928bd9063007edaf4ca37e434b62155671a9f76
Shubham Shekhar <shubhamshekhar123.ss@gmail.com>
2021-04-13 19:33:41 +0000

    final-commit

--- on_call/settings.py
+++ on_call/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "a_static")
+    os.path.join(BASE_DIR, "a_static"),
     os.path.join(BASE_DIR, 'medical-ai', "build", "static"),
 )
 
